17818043339b8146dc8b082935efcd29bb62e11f
yan_santana <yansantana63@gmail.com>
2025-04-19 06:08:52 -0400

    add: config box Material-UI

--- src/components/formPreview.jsx
+++ src/components/formPreview.jsx
@@ -8,14 +8,16 @@ const FormPreview = ({ schema, uischema, data, onChange }) => {
       <Typography variant='h6' gutterBottom sx={{ fontWeight: 'bold' }}>
         Visualização do formulário JSON renderizado
       </Typography>
-      <JsonForms
-        schema={schema}
-        uischema={uischema}
-        data={data}
-        onChange={({ data }) => onChange(data)}
-        renderers={materialRenderers}
-        cells={materialCells}
-      />
+      <Box sx={{ maxHeight: 'calc(100vh - 200px)', overflowY: 'auto', pr: 2 }}>
+        <JsonForms
+          schema={schema}
+          uischema={uischema}
+          data={data}
+          onChange={({ data }) => onChange(data)}
+          renderers={materialRenderers}
+          cells={materialCells}
+        />
+      </Box>
     </Paper>
   );
 };
